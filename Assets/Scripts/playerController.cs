edc80d96cb661bd3ff3cf5d23aa393eee7ecaad7
Alex <alexpalvarez@protonmail.com>
2020-07-01 17:31:16 +0200

    Esto arranca, parece que vamos

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public Vector2 position;
+
+
+
+    void OnMouseDown(){
+        Debug.Log("hey negro");
+        position.x ++;
+        transform.Translate(position  * Time.deltaTime);
+    }
+
+}
