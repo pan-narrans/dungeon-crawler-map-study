e94a3eb2ec5072b13a9f1aed1cbe0f3300ab96c2
Omaki <diegoruizcosquer@yahoo.es>
2020-07-01 19:55:21 +0200

    Ya tenemos cuadraos k se posicionan al tun tun

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        // Debug.Log("hello world");
-    // }
+     //Start is called before the first frame update
+     void Start()
+     {
+         Debug.Log("hello world");
+		 transform.position = new Vector2(Random.Range(-14,14),Random.Range(-8,8));
+     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
+    //Update is called once per frame
+     void Update()
+     {
         
-    // }
+     }
 }
