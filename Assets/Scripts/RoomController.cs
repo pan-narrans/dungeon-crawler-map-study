00efc1dda6dcf3e5762138fc1b4e3373b67032c9
Omaki <diegoruizcosquer@yahoo.es>
2020-07-02 18:09:43 +0200

    Cuadrao Lefao

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+public class RoomController : MonoBehaviour
 {
      //Start is called before the first frame update
+	 public GameObject Room;
+	
      void Start()
      {
          Debug.Log("hello world");
-		 transform.position = new Vector2(Random.Range(-14,14),Random.Range(-8,8));
+		// transform.position = new Vector2(Random.Range(-14,14),Random.Range(-8,8));
      }
 
     //Update is called once per frame
      void Update()
      {
-        
+        Instantiate(Room);
+		Room.transform.position = new Vector2(Random.Range(-9,10),Random.Range(-9,10));
      }
 }
